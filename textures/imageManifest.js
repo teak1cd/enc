5e4d023a95f59b6ca131218df8cc57be38f593d2
teak1cd <isenne@sterneschool.org>
2016-10-19 13:45:53 -0700

    Create imageManifest.js

---
+++ textures/imageManifest.js
@@ -0,0 +1,20 @@
+function getImageManifest(){
+return {
+  "files":["none"],
+    "none":{
+      "path":"//:0"
+    }
+  };
+}
+/*
+below you can see the format for adding images.
+{
+  "files":["none"],
+    "none":{
+      "path":"//:0"
+    },
+    "imageTitle":{
+      "path":"image path relative to index.html",
+    }
+  }
+  */
