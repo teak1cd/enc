faea0c31bcf94c8055bf3f310787bedf0d98c7e8
teak1cd <isenne@sterneschool.org>
2016-10-19 08:04:35 -0700

    Update sprite.js

--- js/datatype/sprite.js
+++ js/datatype/sprite.js
@@ -8,13 +8,35 @@ function sprite(ARR_anim,ARR_frame,Sprite_sheet){
   this.animationFinishAction = "none";
   this.nextAnimation="";
 }
+
+sprite.prototype.type = "sprite";
+
+sprite.prototype.finishActions = {"none":"none","repeat":"repeat","setAnimation":"setAnimation"};
+
+sprite.prototype.default(){
+  this.animationLoop[0] = this.animationLoop[1];
+  this.animationFinishAction = this.finishActions.none;
+  console.warn("defaulting on frame "+this._frame);
+};
+
 sprite.prototype._tick = function(){
-  this._frame++;
   if(this._frame>=this.animationLoop[1]){
     if(this.animationFinishAction==="repeat"){
       this._frame=this.animationLoop[0];
     }else if(this.animationFinishAction==="setAnimation"){
-      
+      if(this.nextAnimation===""){
+        console.warn("missing next animation, defaulting to action:\"none\"");
+        this.default();
+      }
+      this._animationLoop[0] = this.animations[this.nextAnimation].startFrame;
+      this._animationLoop[1] = this._animationLoop[0]+this.animations[this.nextAnimation].length;
+      this.nextAnimation = "";
     }
+  }else{
+    this._frame++;
   }
+};
+
+sprite.prototype.getFrame = function(){
+ return new frame();
 }
